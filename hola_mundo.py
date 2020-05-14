371283960186fe86279ce7e9e76f1e27dcc14248
JuanJose <42131930+AllMigth@users.noreply.github.com>
2020-05-14 09:53:19 -0500

    Primeras lineas de codigo con hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)  #nuevo objeto
+@app.route('/')     #decorador o wrap  RUTAS A LAS QUE EL USUARIO PUEDE ENTRAR
+def index():
+    return 'Hola mundo'         #Regresar un string
+
+app.run()   #se encarga de ejecutar nuestro servidor en el puerto 5000
